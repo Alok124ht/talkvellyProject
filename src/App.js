128b40c9652df587404e6cc1d4006882663bff20
alok gautam <agautam2285@gmail.com>
2023-04-24 11:19:16 +0530

    frontendpush

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { data } from './data.js';
 
 function App() {
+  const [contacts, setContacts] = useState(data);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('localhost:5000/getAllads') // replace with the URL of your API endpoint
+      .then(response => response.json())
+      .then(data => setContacts(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1 className='text-center mt-4'>ADS</h1>
+        <Form>
+          <InputGroup className='my-3'>
+
+            {/* onChange for search */}
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search contacts'
+            />
+          </InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>companyId</th>
+              <th>PrimaryText</th>
+              <th>Headline</th>
+              <th>Description</th>
+              <th>CTA</th>
+              <td>imageUrl</td>
+
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              .filter((item) => {
+                return search.toLowerCase() === ''
+                  ? item
+                  : item.primaryText.toLowerCase().includes(search);
+              })
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.companyId}</td>
+                  <td>{item.primaryText}</td>
+                  <td>{item.headline}</td>
+                  <td>{item.description}</td>
+                  <td>{item.CTA}</td>
+                  <td>{item.imageUrl}</td>
+
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const data =[
+
+
+{
+id :1,
+companyId: 1,
+primaryText:"The world's leading CRM is ready to help you simplify the business par…",
+headline:"Salesforce for Small Business",
+description:"anything",
+CTA:"sign up",
+imageUrl:"https://drive.google.com/file/d/17huYmoSHdbgcNqfoO4yYYGIFf8X1243T/view…"
+},
+
+
+{
+companyId :2,
+primaryText :"We like where you’re going with this",
+headline: "Relaxed Fit Men's Jeans",
+description:"anything",
+CTA :"shop Now",
+imageUrl:"https://drive.google.com/file/d/17kQiqGnkLEZZmnzLlWG7ZIlN6XbwAVfb/view…"
+},
+
+{
+    companyId: 10,
+    primaryText : "Say ‘goodnight’ to sleeping hot 🔥 with Purple products—designed to di…",
+    headline: "Say ‘goodnight’ to sleeping hot 🔥 with Purple products—designed to di…",
+    description: "Shop Purple products, designed to help you sleep cool.",
+    CTA: "Order Now",
+    imageUrl: "https://drive.google.com/file/d/17vrlQMchymHqlN35p4os23jYqQjFiUNq/view…",
+    
+},
+
+{
+    companyId: 11,
+    primaryText : "Dark spots. Breakouts. Rosacea. Dull skin. Fine lines. Our formulas are custom-mixed for YOUR skin concerns",
+    headline: "Personalized skincare for dark spots, acne, and more.",
+    description: "Personalized skincare for dark spots, acne, and more. Results may vary.",
+    CTA: "Order Now",
+    imageUrl: "https://drive.google.com/file/d/17vzdu8jSulXgzk9rkJaHP7W1940pQaAV/view?usp=sharing"
+},
+
+
+
+
+]
